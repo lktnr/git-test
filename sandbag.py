d6c2a3da269295e47c9e487757a0b9dd94889edf
Hernichtocles <philiphernicht@gmail.com>
2025-03-27 22:48:19 +0100

    asking the user a numberand selecting from  a list

--- sandbag.py
+++ sandbag.py
@@ -1,5 +1,8 @@
+9
 print("Hello! What did you climb")
 
 grade = str(input())
-
-print("Okay. That's an easy warmup...noob")
+print("How did you feel on a scale of 0 to 2?")
+number=int(input())
+list=["Okay. That's an easy warmup...noob", "Wow, that's absolutely crazy, congrats!", "Meh even my Grandma climbed two grades harder yesterday!"]
+print(list[number])
